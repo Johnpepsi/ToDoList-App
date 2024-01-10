c7d503d5eafad14a8e680598b4cb125b2ce5656c
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-01-10 12:04:01 -0600

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>To Do List ✓</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+
+// == Challenges == //
+
+// Part 1. Challenge 1. Render all the notes insides notes.js as a seperate Note component.
+
+// Part 3. Challenge 1. Implement the add note functionality.
+// Create a constant that keeps track of the title and content.
+// Pass the new note back to the app.
+// Add new note to an array.
+// Take array and render sepearete nttoe compionetn for each item.
+
+// Part 3. Challenge 2. Implement the delete note functionality.
+// Callback from the note component to trigger a delete function.
+// Use the filter function out the item that needs deletion.
+// Pass a id over to the note component, pass it back to the App when deleting.
